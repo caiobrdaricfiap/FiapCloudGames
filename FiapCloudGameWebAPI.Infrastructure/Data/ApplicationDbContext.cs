93d6ad05e4dcbb3625bc75bdb671f972a2e801c5
Gustavo <guguren3@hotmail.com>
2025-05-12 13:15:10 -0300

    comments removed

--- FiapCloudGameWebAPI.Infrastructure/Data/ApplicationDbContext.cs
+++ FiapCloudGameWebAPI.Infrastructure/Data/ApplicationDbContext.cs
@@ -19,37 +19,4 @@ namespace FiapCloudGames.Infrastructure.Data
         }
     }
 
-//    using FiapCloudGames.Domain.Entities;
-//using FiapCloudGameWebAPI.Models;
-//using Microsoft.EntityFrameworkCore;
-
-//namespace FiapCloudGames.Infrastructure.Data
-//    {
-//        public class ApplicationDbContext : DbContext
-//        {
-//            private readonly string _connectionString;
-
-//            public ApplicationDbContext() { }
-//            public ApplicationDbContext(string connectionString)
-//            {
-//                _connectionString = connectionString;
-//            }
-
-//            public DbSet<UserModel> Users { get; set; }
-//            public DbSet<UserModel> Games { get; set; }
-//            public DbSet<GamesLibraryModel> GamesLibrary { get; set; }
-
-//            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//            {
-//                if (!optionsBuilder.IsConfigured)
-//                    optionsBuilder.UseSqlServer(_connectionString);
-//            }
-
-//            protected override void OnModelCreating(ModelBuilder modelBuilder)
-//            {
-//                modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
-//            }
-
-//        }
-//    }
 }
